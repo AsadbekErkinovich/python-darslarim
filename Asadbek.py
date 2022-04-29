bae6e46f437e4086b480427c88b9af48413c301a
AsadbekErkinovich <asadbekno020@gmail.com>
2022-04-29 13:10:47 +0500

    Darslar uchun qo`llanma
    Bu yerda siz turli hil mashqlarni javoblarini ko`rasiz

---
+++ Asadbek.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 18:54:18 2022
+
+@author: User
+"""
+
+#ism="Anvar"
+#yosh=16
+#xabar= ism+ " " + str(yosh)+ "yoshda"
+#print(xabar)
+#t_yil=int(input("Tug`ulgan yilingizni kiriting : "))
+#yosh=2022-t_yil
+#print("Siz ", yosh, " yoshda ekansiz")
+#mevalar=["olma", "nok","mandarin"]
+#narxlar=[1200,6400,5600,4100]
+#print(mevalar)
+#sonlar=["bir","ikki","uch",1,2,3]
+#print(sonlar)
+#sonlar=[14,1,21,-4,-8,65,56,65]
+#print(sorted(sonlar))
+#toys=("bus","snake","dino","lizart")
+#for muhmon in muhmonlar :
+    #print('salom',muhmon)
+    #print('xayr',muhmon)
+#sonlar=list(range(1,11))
+#for son in sonlar:
+#  print(f"{son} ning kvadrati {son**2} ga teng")  
+#sonlar=list(range(11))
+#sonlar_kvadrati=[]
+#for son in sonlar:
+  #  sonlar_kvadrati.append(son**2)
+#ism=input("Ismingiz nima")
+#print("Assalomu aleykum,"+ ism)   
+#t_yil=int(input("tug`ulgan yilingizni kiriting!"))
+#yosh=2022-t_yil
+#print("Siz", yosh,"da ekansiz")
+#mevalar=['olma','nok','behi','shaftoli']
+#print(mevalar)
+#mehmonlar=['Ali','vali','holi','moli','xuri']
+#for mehmon in mehmonlar:
+ #   print('Salom',mehmon)
+  #  print('Hayr', mehmon)
+#sonlar=list(range(1,11))
+#for son in sonlar:
+   # print(f"{son} 'kvadrati' {son**2} 'ga teng'")
+#sonlar=list(range(10))
+#sonlar_kvadrati=[]
+#for son in sonlar:
+    #sonlar_kvadrati.append(son**2)
+#print(sonlar)
+#print(sonlar_kvadrati)
+#dostlar=[]
+#print("beshta eng yaqin do`stingiz kim")
+#for n in range(5):
+   # dostlar.append(input(f"{n+1}-dostingizni kiriting"))
+#print(dostlar)    
+#avtolar=['mers','audi','lambargini','bmw','lexus']                  
+#for avto in avtolar:
+   # if avto == 'bmw':
+     #   print(avto.upper())
+   # else:
+     #   print(avto.title()
+#ism=input('ismingiz nima? \n>>>')
+#if ism.lower() != 'ali':
+  #print(f"uzr,{ism.title()} biz Alini kutyapmiz.")
+#else:
+   # print("Salom ali")
+#javob=float(input("12*6 nechiga teng?>>>"))
+#if javob !=72:
+    #print("kechirasiz chichib qo`ydiz!")
+#else:
+   # print("malades cho`chqacham1")    
+#son=-72
+#if son<0:
+   # print("manfiy son")
+#else:
+    #print('musbat son')
+#yosh=int(input("Yoshingizni kiriting >>>"))
+#if yosh<4:
+  #  print("sizga kirish bepul")
+#elif yosh<=12:
+   # print("Sizga kirish 5000 so`m!1")
+#else:
+   # print("To`la naxxuy pulni!")
+#kun=input("bugun nima kun? ")
+#harorat=float(input("Haroratni kiriting "))
+
+#if (kun.lower()=='Yakshanba' or kun.lower()=='shanba') and harorat>=30:
+    #print("Cho`milgani ketdik!")
+#elif (kun.lower()=='Yakshanba' or kun.lower()=='shanba') and harorat<30:
+    #print("Uyingda o`tir")
+#narh=15000
+#choy=True
+#salat=True
+#if choy and salat:
+   # narh= narh + 10000
+#elif choy or salat:
+    #narh= narh + 5000
+#print(f'jami {narh} som')    
+#menu=["osh","manti","qozonkabaob","dimlama","moshxorda"]
+#ovqat=input('Nima ovqat yeysiz ')
+#if ovqat.lower() in menu:
+   # print("Buyurtma qabul qilindi")
+#else:
+   # print("Avsuski bizda bunday ovqat yo`q")
+#buyurtmalar=['lagmon','shorva','chuchvara','manti']
+#for taom in buyurtmalar:
+ #   if taom in menu:
+#        print(f"menuda {taom} bor")
+#    else:
+#        print(f"kechirasiz, menuda {taom} yo`q")
+#menu=['manti','somsa','osh','lag`mon','norin']
+#ovqat=input('nima ovqat yiysiz?')
+#if ovqat in menu:
+ #   print('Buyurtmangiz qabul qilindi ')
+#else:
+    #print('Uzur bizda bunday taom yo`q')
+#buyurtmalar=['chuchvara','lag`mon','manti','osh']
+
+#for taom in buyurtmalar:
+    #if taom in menu:
+    #  print(f'Ha bizda {taom} bor')
+    #else:      
+     # print('sizga boshqa ovqatlarimizni taklif qilib qolamiz')
+son=float(input("Istalgan sonni kiriting"))
+ildiz=son**(1/3)
+print(f"{son} ning ildizi {ildiz} ga teng")    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
